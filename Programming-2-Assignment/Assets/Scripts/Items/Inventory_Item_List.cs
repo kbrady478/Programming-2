2f6e449ec40cda480cfdd3b26f4473b47d347386
kb478 <c23789389@mytudublin.ie>
2024-11-25 15:02:46 +0000

    Adding to inventory functional, adds to actual list instead of ui

--- Programming-2-Assignment/Assets/Scripts/Items/Inventory_Item_List.cs
+++ Programming-2-Assignment/Assets/Scripts/Items/Inventory_Item_List.cs
@@ -6,11 +6,17 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public struct Inventory_Object
+{
+    public Inventory_Item_Data data;
+    public GameObject game_Object;
+}
+
 public class Inventory_Item_List : MonoBehaviour
 {
     [SerializeField] private GameObject content = default;
     [SerializeField] private GameObject menu_Item_Template = default;
-    [SerializeField] private List<object> inventory_List = default;
+    private List<Inventory_Object> inventory_List = new List<Inventory_Object>();
 
 
     private void Start()
@@ -29,10 +35,35 @@ public class Inventory_Item_List : MonoBehaviour
         }   
     }// end Populate_Inventory_Item()
 
+    private void Update()
+    {
+        // just for testing list
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (inventory_List.Count == 0) return;
+            
+            for (int i = 0; i < inventory_List.Count; i++)
+            {
+                Debug.Log(inventory_List[i].data.name);
+            } 
+        }
+    }
+
     // Add item to the inventory list, called when picking up new item
     public void Add_Item_To_Inventory(Inventory_Item_Data item_Data, GameObject item_Object)
     {
+        // Assign item data and transform to struct
+        Inventory_Object new_Inventory_Object = new Inventory_Object();
         
+        new_Inventory_Object.data = item_Data;
+        new_Inventory_Object.game_Object = item_Object;
+        
+        // Add to list, relocate off-screen to inventory transform
+        inventory_List.Add(new_Inventory_Object);
+        new_Inventory_Object.game_Object.transform.SetParent(gameObject.transform);
+        new_Inventory_Object.game_Object.transform.localPosition = Vector3.zero;
+        
+        // Add listing to inventory screen    
         GameObject new_Item_Listing = Instantiate(menu_Item_Template, transform.position, transform.rotation);
 
         string label = $"   {item_Data.name}";
@@ -42,6 +73,7 @@ public class Inventory_Item_List : MonoBehaviour
         new_Item_Listing.GetComponentInChildren<TextMeshProUGUI>().text = label;
         // Reset scale because it goes weird
         new_Item_Listing.transform.localScale = Vector3.one;
+        
     }// end Add_Item_To_Inventory()
     
 }// end script
